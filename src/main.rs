787104c59dbae232a1c8422ce6b0783ab84c9b87
Lummy <67608060+cllojan@users.noreply.github.com>
2022-07-21 19:20:04 +0000

    windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{conf, event, Context, GameResult};
+use std::path;
+
+struct Game{}
+
+impl event::EventHandler<ggez::GameError> for Game{
+    fn update(&mut self, _context:&mut Context) -> GameResult{
+        Ok(())
+    }
+
+    fn draw(&mut self, _context:&mut Context)-> GameResult{
+        Ok(())
+    }
+}
+pub fn main() -> GameResult{
+    let context_builder = ggez::ContextBuilder::new("rust_sokoban","sokoban")
+        .window_setup(conf::WindowSetup::default().title("Rust Sokoban"))
+        .window_mode(conf::WindowMode::default().dimensions(800.0,600.0))
+        .add_resource_path(path::PathBuf::from("./resources"));
+    let (context, event_loop) = context_builder.build()?;
+    // Create the game state
+    let game = Game {};
+    // Run the main event loop
+    event::run(context, event_loop, game)
 }
